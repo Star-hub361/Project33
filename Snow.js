189d9c76d3244303c94d019bec84e1a11105ad40
Star-hub361 <74688129+Star-hub361@users.noreply.github.com>
2021-08-03 14:13:50 -0400

    Add files via upload

---
+++ Snow.js
@@ -0,0 +1,8 @@
+class Snow {
+    constructor(x, y) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0 }
+        }
+    }
---
+++ sketch.js
@@ -0,0 +1,15 @@
+var backgroundImg
+
+function preload(){
+backgroundImg = loadImage("sprites/snow3.jpg");
+}
+
+  function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() { 
+  background(backgroundImg);
+  drawSprites();
+}
